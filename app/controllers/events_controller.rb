20581d0b0ebdbe5e2a7b0e2816789e4652f4e2e4
phthallo <phthallo@hackclub.app>
2024-08-13 11:44:20 +0000

    add: image upload functionality

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
     @events = Event.all
   end
 
-  def read
+  def show
     @event = Event.find(params[:id])
   end
 
@@ -19,6 +19,31 @@ class EventsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
 
+  def edit
+    @event = Event.find(params[:id])
+    @event.images.attach(params[:images])
+  end
 
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+    def event_params
+      params.require(:event).permit(:title, :body, images: [])
+    end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,3 @@
 class Event < ApplicationRecord
+    has_many_attached :images
 end
